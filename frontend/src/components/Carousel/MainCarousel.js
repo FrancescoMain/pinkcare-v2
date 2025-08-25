a95376faa15b088834766249865e884854c7d1f3
francescoMain <cesaranofrancescomain@gmail.com>
2025-08-25 13:21:57 +0200

    fix deploy

--- frontend/src/components/Carousel/MainCarousel.js
+++ frontend/src/components/Carousel/MainCarousel.js
@@ -1,29 +1,14 @@
-import React from "react";
-import Carousel from "./Carousel";
+import React, { useEffect } from "react";
 
 const MainCarousel = () => {
-  const slides = [
-    {
-      src: "/styles/public/images/slider/slide_pink-care_web-1.jpg",
-      alt: "PinkCare",
-    },
-    {
-      src: "/styles/public/images/slider/slide_pink-care_web-2.jpg",
-      alt: "Programmi di prevenzionale personalizzata",
-    },
-    {
-      src: "/styles/public/images/slider/slide_pink-care_web-3.jpg",
-      alt: "Specialisti accreditati pinkcare",
-    },
-    {
-      src: "/styles/public/images/slider/slide_pink-care_web-4.jpg",
-      alt: "Salute e benessere al femminile",
-    },
-    {
-      src: "/styles/public/images/slider/slide_pink-care_web-5.jpg",
-      alt: "Affilizione specialista",
-    },
-  ];
+  useEffect(() => {
+    // Initialize Bootstrap carousel if jQuery and Bootstrap are loaded
+    if (window.jQuery && window.jQuery.fn.carousel) {
+      window.jQuery("#myCarousel").carousel({
+        interval: 5000,
+      });
+    }
+  }, []);
 
   return (
     <>
@@ -31,14 +16,76 @@ const MainCarousel = () => {
         Con PinkCare potrai ottenere gratuitamente il tuo Piano di prevenzione e
         cura
       </h4>
-      <Carousel
-        items={slides}
-        autoPlay={true}
-        interval={5000}
-        showIndicators={true}
-        showControls={true}
-        className="main-carousel"
-      />
+      {/* Start carousel - IDENTICAL to JSF original */}
+      <div id="myCarousel" className="carousel slide" data-ride="carousel">
+        {/* Indicators */}
+        <ol className="carousel-indicators">
+          <li
+            data-target="#myCarousel"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+          <li data-target="#myCarousel" data-slide-to="4"></li>
+        </ol>
+
+        {/* Wrapper for slides */}
+        <div className="carousel-inner">
+          <div className="item active">
+            <img
+              src="/styles/public/images/slider/slide_pink-care_web-1.jpg"
+              alt="PinkCare"
+            />
+          </div>
+
+          <div className="item">
+            <img
+              src="/styles/public/images/slider/slide_pink-care_web-2.jpg"
+              alt="Programmi di prevenzionale personalizzata"
+            />
+          </div>
+
+          <div className="item">
+            <img
+              src="/styles/public/images/slider/slide_pink-care_web-3.jpg"
+              alt="Specialisti accreditati pinkcare"
+            />
+          </div>
+          <div className="item">
+            <img
+              src="/styles/public/images/slider/slide_pink-care_web-4.jpg"
+              alt="Salute e benessere al femminile"
+            />
+          </div>
+          <div className="item">
+            <img
+              src="/styles/public/images/slider/slide_pink-care_web-5.jpg"
+              alt="Affilizione specialista"
+            />
+          </div>
+        </div>
+
+        {/* Left and right controls */}
+        <a
+          className="left carousel-control"
+          href="#myCarousel"
+          data-slide="prev"
+        >
+          <span className="glyphicon glyphicon-chevron-left"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="right carousel-control"
+          href="#myCarousel"
+          data-slide="next"
+        >
+          <span className="glyphicon glyphicon-chevron-right"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+      {/* End carousel */}
     </>
   );
 };
--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 import PageHead from "../../layout/PageHead";
 import { getPageConfig } from "../../../config/pageConfig";
-import { MainCarousel, NewsCarousel } from "../../Carousel";
+import { MainCarousel } from "../../Carousel";
 
 const Home = () => {
   const pageData = getPageConfig("home");
@@ -19,64 +19,7 @@ const Home = () => {
         links={pageData.links || []}
       />
       <div className="home-page">
-        {/* Start Main Container Area */}
-        <div className="container main-container" style={{marginTop: '30px'}}>
-          <div className="row">
-            {/* Start Appointment Image Area */}
-            <div className="col-md-6 col-xs-12">
-              <MainCarousel />
-            </div>
-            <div className="col-md-1 col-sm-12 col-xs-12"></div>
-            {/* End Appointment Image Area */}
-            
-            {/* Start Content Area */}
-            <div className="col-md-5 col-sm-12 col-xs-12">
-              <div>
-                <h4 className="main-heading2" style={{marginBottom: '30px'}}>
-                  <i className="fas fa-pencil-square-o"></i>
-                  Iscriviti*
-                </h4>
-                <p>*Iscrizione gratuita</p>
-                <p>Scopri PinkCare - il portale dedicato alla salute femminile</p>
-              </div>
-            </div>
-            {/* End Content Area */}
-          </div>
-        </div>
-        {/* End Main Container Area */}
-
-        {/* Example News Carousel */}
-        <div className="container main-container">
-          <div className="row">
-            <section className="col-md-8 col-sm-12 col-xs-12">
-              <NewsCarousel 
-                newsData={[
-                  // Example news data - in real app this would come from props/state
-                  [
-                    {
-                      id: 1,
-                      headline: "Prevenzione del cancro al seno",
-                      text: "Scopri l'importanza della prevenzione...",
-                      image: "/styles/public/images/news-post-img-1.png",
-                      insertion_date: new Date().toISOString()
-                    },
-                    {
-                      id: 2,
-                      headline: "Alimentazione in gravidanza", 
-                      text: "Consigli nutrizionali per la gravidanza...",
-                      image: "/styles/public/images/news-post-img-2.png",
-                      insertion_date: new Date().toISOString()
-                    }
-                  ]
-                ]}
-                resourceBundle={{
-                  Last_News: "Ultime News",
-                  Read_all: "Leggi tutto"
-                }}
-              />
-            </section>
-          </div>
-        </div>
+        <MainCarousel />
       </div>
     </>
   );
